7c34fde8acbb5d9ff836cb1c33be84bb3048e180
danialqureshi <danialqureshi@192.168.0.16>
2018-09-18 23:01:38 -0400

    updated online store

--- Applestoreproj/src/AppleStore.java
+++ Applestoreproj/src/AppleStore.java
@@ -1,10 +1,10 @@
 
 public class AppleStore {
-	
+
 	public static void main(String[] args) {
-		
+
 		ApplestoreFrame frame = new ApplestoreFrame();
 		frame.Framee();
-		
+
 	}
 }
--- Applestoreproj/src/datastructures/hashmapss.java
+++ Applestoreproj/src/datastructures/hashmapss.java
@@ -1,13 +1,14 @@
 package datastructures;
+
 import java.util.HashMap;
 
 public class hashmapss {
 
-	HashMap<String, Integer> map = new HashMap<String,Integer>();
-	HashMap<String, Integer> macmap2 = new HashMap<String,Integer>();
-	HashMap<String,Integer> accessoriesmap2 = new HashMap<String,Integer>();
-	
-	public HashMap<String, Integer> getiphonehash(){
+	HashMap<String, Integer> map = new HashMap<String, Integer>();
+	HashMap<String, Integer> macmap2 = new HashMap<String, Integer>();
+	HashMap<String, Integer> accessoriesmap2 = new HashMap<String, Integer>();
+
+	public HashMap<String, Integer> getiphonehash() {
 		map.put("iPhone SE", 469);
 		map.put("iPhone 6", 499);
 		map.put("iPhone 6s", 599);
@@ -17,20 +18,20 @@ public class hashmapss {
 		map.put("iPhone 8", 929);
 		map.put("iPhone 8 Plus", 1059);
 		map.put("iPhone X", 1319);
-		
+
 		return map;
 	}
-	
-	public HashMap<String,Integer> getmachashmap() {
-		macmap2.put("MacBook" , 1729);
-		macmap2.put("MacBook Air" , 1199);
-		macmap2.put("MacBook Pro" , 2399);
-		macmap2.put("iMac" , 1399);
-		macmap2.put("iMac Pro" , 6299);
+
+	public HashMap<String, Integer> getmachashmap() {
+		macmap2.put("MacBook", 1729);
+		macmap2.put("MacBook Air", 1199);
+		macmap2.put("MacBook Pro", 2399);
+		macmap2.put("iMac", 1399);
+		macmap2.put("iMac Pro", 6299);
 		return macmap2;
 	}
-	
-	public HashMap<String,Integer> getaccesoriesmap(){
+
+	public HashMap<String, Integer> getaccesoriesmap() {
 		accessoriesmap2.put("Earpods", 35);
 		accessoriesmap2.put("Airpods", 219);
 		accessoriesmap2.put("USB Charging Cable", 35);
